18e8f56fb251b871c9909ff1c4d1b7b4459de414
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-11-06 12:28:59 -0300

    commit :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'package:yes_no_app/config/theme/app_theme.dart';
-import 'package:yes_no_app/presentation/providers/chat_provider.dart';
-import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,15 +8,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ChatProvider() )
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Yes No App',
-        theme: AppTheme(selectedColor: 6).theme(),
-        home: const ChatScreen()
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 4).theme(),
+      title: 'Yes No App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton.tonal(
+            onPressed: (){},
+            child: const Text('Click me')),
+        ),
       ),
     );
   }
