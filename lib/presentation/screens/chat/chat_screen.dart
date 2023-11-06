35f56c62a784df3d301eb4d0ad0a89a980d130aa
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-11-06 15:48:02 -0300

    commit :)

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Anya 💕'),
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://img.20mn.fr/NiDWAVF4SaSODyTM6bNfeyk/1200x768_actrice-anya-taylor-joy'),
+          ),
+        ),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Column(children: [
+          Expanded(child: ListView.builder(
+            itemCount: 100,
+            itemBuilder: (context, index) {
+            return const MyMessageBubble();
+          })),
+        
+          const Text('Text Area')
+        ]),
+      ),
+    );
+  }
+}
