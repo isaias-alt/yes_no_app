8913e1cd038d1f5b832d1930cdc570c0074436b6
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-11-08 19:51:55 -0300

    commit :)

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+  List<Message> messageList = [];
+
+  Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+    final newMessage = Message(text: text, fromWho: FromWho.mine);
+    messageList.add(newMessage);
+    notifyListeners();
+    moveScrollToBotton();
+  }
+
+  Future<void> moveScrollToBotton() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
+  }
+}
--- lib/presentation/widgets/chat/my_message_bubble.dart
+++ lib/presentation/widgets/chat/my_message_bubble.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class MyMessageBubble extends StatelessWidget {
-  const MyMessageBubble({super.key});
+  final Message message;
+  const MyMessageBubble({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +17,15 @@ class MyMessageBubble extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
             color: colors.primary,
           ),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Text('My Message', style: TextStyle( color: Colors.white ),),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              message.text,
+              style: const TextStyle(color: Colors.white),
+            ),
           ),
         ),
-        const SizedBox(height:10),
+        const SizedBox(height: 5),
       ],
     );
   }
