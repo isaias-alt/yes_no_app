28688883380ec314b18b192c745aa9d60fb608e8
isaias-alt <cascolucasisaias@gmail.com>
2024-09-12 18:11:16 -0400

    cambios

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
@@ -30,8 +31,9 @@ class ChatProvider extends ChangeNotifier {
   Future<void> moveScrollToBotton() async {
     await Future.delayed(const Duration(milliseconds: 100));
     chatScrollController.animateTo(
-        chatScrollController.position.maxScrollExtent,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.bounceInOut);
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.bounceInOut,
+    );
   }
 }
