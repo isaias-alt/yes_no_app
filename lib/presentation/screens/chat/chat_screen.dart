3a7b60f26978ad8afea6ffb15cb283260a705b62
isaias-alt <cascolucasisaias@gmail.com>
2024-09-12 15:21:12 -0400

    cambios

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'package:yes_no_app/domain/entities/message.dart';
-import 'package:yes_no_app/presentation/providers/chat_provider.dart';
-import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
-import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
-import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+import 'package:yes_no_app/presentation/views/chat/chat_view.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -24,41 +18,7 @@ class ChatScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: _ChatView(),
-    );
-  }
-}
-
-class _ChatView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final chatProvider = context.watch<ChatProvider>();
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 5),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                controller: chatProvider.chatScrollController,
-                itemCount: chatProvider.messageList.length,
-                itemBuilder: (context, index) {
-                  final message = chatProvider.messageList[index];
-                  return (message.fromWho == FromWho.hers)
-                      ? HerMessageBubble(
-                          message: message,
-                        )
-                      : MyMessageBubble(
-                          message: message,
-                        );
-                },
-              ),
-            ),
-            MessageFieldBox(onValue: chatProvider.sendMessage),
-            const SizedBox(height: 5)
-          ],
-        ),
-      ),
+      body: const ChatView(),
     );
   }
 }
